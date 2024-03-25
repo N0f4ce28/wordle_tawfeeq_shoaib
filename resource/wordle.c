6ae0fbb9231943227573a4594ac569d424acb6fd
Squatwart <161691032+Squatwart@users.noreply.github.com>
2024-03-25 15:31:43 +0400

    wordle_script

--- resource/wordle.c
+++ resource/wordle.c
@@ -1,56 +1,145 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 
-#define MAX_WORD_LENGTH 20
-#define MAX_WORD_COUNT 2309
+#define MAX_TRIES 6
+#define MAX_WORD_LENGTH 26
 
-int main() {
-    // Ouvrir le fichier contenant les mots Wordle
-    FILE *file = fopen("bdd_wordle.txt", "r");
-    if (file == NULL) {
-        perror("Erreur lors de l'ouverture du fichier");
+int check_guess(char guess[], char target[]);
+void print_word(char word[], int length);
+
+int main()
+{
+    char target[MAX_WORD_LENGTH + 1], guess[MAX_WORD_LENGTH + 1];
+    int correct_letter;
+    int try = 1;
+    int play_again = 1;
+
+    FILE *fp;
+    fp = fopen("bdd_wordle.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file.\n");
         return 1;
     }
 
-    // Allouer de la mémoire pour stocker les mots
-    char *word_list = (char *)malloc(MAX_WORD_COUNT * sizeof(char *));
-    if (word_list == NULL) {
-        perror("Erreur lors de l'allocation de mémoire");
-        return 1;
-    }
-
-    // Initialiser la liste des mots
-    for (int i = 0; i < MAX_WORD_COUNT; i++) {
-        word_list[i] = (char *)malloc(MAX_WORD_LENGTH * sizeof(char));
-        if (word_list[i] == NULL) {
-            perror("Erreur lors de l'allocation de mémoire");
-            return 1;
-        }
-    }
-
-    // Lire les mots du fichier et les stocker dans la liste
+    // Count the number of words in the file
     int word_count = 0;
-    char word[MAX_WORD_LENGTH];
-    while (fscanf(file, "%s", word) != EOF) {
-        strcpy(word_list[word_count], word);
+    char word[MAX_WORD_LENGTH + 1];
+    while (fscanf(fp, "%s", word) != EOF) {
         word_count++;
     }
 
-    // Fermer le fichier
-    fclose(file);
+    // Allocate memory for the words
+    char **words = malloc(word_count * sizeof(char *));
+    if (words == NULL) {
+        printf("Memory allocation error.\n");
+        return 1;
+    }
 
-    // Afficher les mots stockés dans la liste
-    printf("Mots stockés :\n");
+    // Rewind the file pointer to read the words
+    rewind(fp);
+
+    // Read words from the file
     for (int i = 0; i < word_count; i++) {
-        printf("%s\n", word_list[i]);
+        words[i] = malloc(MAX_WORD_LENGTH + 1);
+        if (words[i] == NULL) {
+            printf("Memory allocation error.\n");
+            return 1;
+        }
+        fscanf(fp, "%s", words[i]);
     }
 
-    // Libérer la mémoire allouée pour la liste des mots
-    for (int i = 0; i < MAX_WORD_COUNT; i++) {
-        free(word_list[i]);
+    fclose(fp);
+
+    printf("Welcome to Wordle!\n");
+    printf("Instructions:\n");
+    printf("The color of a letter will change to show you how close your guess was.\n");
+    printf("If the letter is green, the letter is in the word and it is in the correct spot.\n");
+    printf("If the letter is yellow, the letter is in the word, but it is not in the correct spot.\n");
+    printf("If the letter is grey, the letter is not in the word.\n");
+    printf("You will have 6 guesses. Good luck!\n\n");
+
+    while (play_again) {
+        // Select target word randomly
+        srand(time(NULL));
+        int index = rand() % word_count;
+        strcpy(target, words[index]);
+
+        int word_length = strlen(target);
+        try = 1;
+
+        printf("The word you are guessing has %d letters.\n\n", word_length);
+
+        // Game loop
+        while (try <= MAX_TRIES) {
+            printf("Try %d: Enter a word: ", try);
+            scanf("%s", guess);
+
+            if (strlen(guess) < word_length) {
+                printf("Word must be %d letters long. Try again.\n", word_length);
+                continue;
+            }
+
+            correct_letter = check_guess(guess, target);
+
+            if (correct_letter == word_length) {
+                printf("Congratulations! You found the word '%s' in %d tries.\n", target, try);
+                break;
+            }
+
+            print_word(guess, word_length);
+            putchar('\n');
+
+            try++;
+        }
+
+        // Display the target word at the end of the game
+        printf("The word was: '%s'\n", target);
+
+        // Ask if the player wants to play again
+        printf("Do you want to play again? (1 = Yes, 0 = No): ");
+        scanf("%d", &play_again);
     }
-    free(word_list);
+
+    printf("Thanks for playing!\n");
+
+    // Free memory allocated for words
+    for (int i = 0; i < word_count; i++) {
+        free(words[i]);
+    }
+    free(words);
 
     return 0;
+}
+
+int check_guess(char guess[], char target[])
+{
+    int i, correct_letter = 0;
+    for (i = 0; i < strlen(target); i++) {
+        if (guess[i] == target[i]) {
+            correct_letter += 1;
+            printf("%c [green] ", guess[i]);
+        } else if (strchr(target, guess[i]) != NULL) {
+            printf("%c [yellow] ", guess[i]);
+        } else {
+            printf("%c [gray] ", guess[i]);
+        }
+    }
+    return correct_letter;
+}
+
+void print_word(char word[], int length)
+{
+    int i;
+    for (i = 0; i < length; i++) {
+        printf("[ ]");
+    }
+    putchar('\n');
+
+    for (i = 0; i < length; i++) {
+        putchar(word[i]);
+        putchar(' ');
+    }
+    putchar('\n');
 }
